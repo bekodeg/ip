94a44d09eee24debf0d9324abe6b2f8f89779e99
bekodeg <andermag102@yandex.ru>
2024-06-21 14:04:27 +0400

    добавил фильтр в репозиторий

--- src/main/java/com/example/exam/repositories/IssueRepository.java
+++ src/main/java/com/example/exam/repositories/IssueRepository.java
@@ -1,10 +1,15 @@
 package com.example.exam.repositories;
 
 import com.example.exam.models.Issue;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface IssueRepository extends JpaRepository<Issue, Long> {
-    Optional<Issue> findByMilestoneId(Long id);
+    Page<Issue> findAllByMilestoneId(Long id, Pageable pageable);
+
+    @NotNull Page<Issue> findAll(@NotNull Pageable pageable);
 }
